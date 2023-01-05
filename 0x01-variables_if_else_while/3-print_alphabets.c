45d6cb40bd6386818d36826062d1be27ef277eb1
XeusAN <Ndukweagbai@hotmial.com>
2023-01-04 17:08:10 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 	char low;
-	
+
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	
+
 	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
-	
-	return ('\n');
+
+	return (0);
 }
